90e1e4523e71709e641abbead1951ba3fde46aba
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-07 23:58:32 +0200

    --

--- Twitter/app/(tabs)/index.tsx
+++ Twitter/app/(tabs)/index.tsx
@@ -1,15 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
+
 import { Text, View } from '@/components/Themed';
+import tweets from '../../assets/data/tweets';
+
+
+const tweet = tweets[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+        <Image src={tweet.user.image}  style={{width: 50, height:50 }}/>
+        <Text> {tweet.content} </Text>
+    </View>      
   );
 }
 
@@ -18,14 +21,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
